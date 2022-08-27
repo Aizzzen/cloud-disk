041feb9653a21418544ce6a8fcbce2d6abefe8f6
Aizzzen <gadamurrr@gmail.com>
2022-08-27 17:58:37 +0300

    add division

--- src/utils/sizeFormat.js
+++ src/utils/sizeFormat.js
@@ -2,7 +2,7 @@ export default (size) => {
     if(size > 1024*1024*1024) {
         return (size/(1024*1024*1024)).toFixed(1)+'GB'
     }
-    if(size > 1024*1024) {
+    if(size > 1024/1024) {
         return (size/(1024*1024)).toFixed(1)+'MB'
     }
     if(size > 1024) {
